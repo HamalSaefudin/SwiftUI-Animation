0c4ee8fd06c88dfc8021ccad5d6533be542b0ed1
Hamal <Hamalsaefudin48@gmail.com>
2022-12-07 19:11:36 +0700

    Initial Commit

---
+++ Swift-Animation/Swift_AnimationApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_AnimationApp.swift
+//  Swift-Animation
+//
+//  Created by Hamal Saefudin on 07/12/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_AnimationApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
